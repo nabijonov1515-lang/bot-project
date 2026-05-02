300548214d7990e7e1380e76c561cd7a0f2729a3
nabijonov1515-lang <nabijonov1515@gmail.com>
2026-05-03 00:58:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = "8345730778:AAECxPUfD47cmjtRmaC8EiI7JWyjSALYYMI"
 
+menu = [["📝 Ariza", "📞 Bog'lanish"]]
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom! Bot ishlayapti 🚀")
+    reply_markup = ReplyKeyboardMarkup(menu, resize_keyboard=True)
+    await update.message.reply_text("Assalomu alaykum!", reply_markup=reply_markup)
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "📝 Ariza":
+        await update.message.reply_text("Ismingizni yozing:")
+    elif text == "📞 Bog'lanish":
+        await update.message.reply_text("📞 Telefon: +998901234567")
+    else:
+        await update.message.reply_text("Tugmadan foydalaning")
 
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, message_handler))
 
 print("Bot ishlayapti...")
 app.run_polling()
