646627ebb3579a17791040fd3db466773e4b0404
nabijonov1515-lang <nabijonov1515@gmail.com>
2026-05-03 00:17:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8345730778:AAEh4BmVsw1GkDoecscU19bVWEMihk0ldT8"
+TOKEN = "8345730778:AAECxPUfD47cmjtRmaC8EiI7JWyjSALYYMI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Bot ishlayapti 🚀")
