8bbb8df9c5aeaee1151744decc7e12b2109fd4c3
nabijonov1515-lang <nabijonov1515@gmail.com>
2026-05-03 21:09:23 +0500

    Initialize Telegram bot with basic command handling

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import os
+import telebot
 
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("TOKEN topilmadi")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Bot ishlayapti ✅")
+
+bot.polling()
